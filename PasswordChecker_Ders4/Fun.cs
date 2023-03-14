acb5c5a0e51b039418ba407a36f82fba1dd83814
ahmet taha mücasiroğlu <48521773+tahamucasiroglu@users.noreply.github.com>
2023-03-14 16:26:51 +0300

    Add files via upload

---
+++ PasswordChecker_Ders4/Fun.cs
@@ -0,0 +1,24 @@
+﻿/*
+ senaryo 
+şifre kontrolü hazırlanacak
+- 6 karakter ve üstünde kontrol yapılacak
+- 3 tip karakter var sayı, harf ve alfanumeric
+- sadece tek tipe sahip ise zayıf, iki tipe sahipse orta ve üç tipede sahipse güçlü olacak
+ */
+
+internal class Program2 // eğlenmek için yapılan hali 
+{
+    private static void Main2(string[] args)
+    {
+        Console.WriteLine("Password:");
+        passwordChecker(Console.ReadLine());
+    }
+
+    private static void passwordChecker(string? message){if (message != null && message.Length >= 6){passwordPowerCheck(message);}else{Console.WriteLine("Undefined");}}
+
+    private static void passwordPowerCheck(string message, int isletter = 0, int isnumber = 0, int isalphanumeric = 0)
+    {
+        foreach (char letter in message){ if (char.IsLetter(letter)) { isletter = 1; } if (char.IsNumber(letter)) { isnumber = 1; } if (char.IsPunctuation(letter)) { isalphanumeric = 1; } }
+        switch (isletter+isalphanumeric+isnumber) { case 1: Console.WriteLine("Weak"); break; case 2: Console.WriteLine("Middle"); break; case 3: Console.WriteLine("Strong"); break; default:Console.WriteLine("Undefined");break;}
+    }
+}
---
+++ PasswordChecker_Ders4/Main.cs
@@ -0,0 +1,81 @@
+﻿/*
+ senaryo 
+şifre kontrolü hazırlanacak
+- 6 karakter ve üstünde kontrol yapılacak
+- 3 tip karakter var sayı, harf ve alfanumeric
+- sadece tek tipe sahip ise zayıf, iki tipe sahipse orta ve üç tipede sahipse güçlü olacak
+ */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            string message = "";
+            Console.WriteLine("Password: for exit enter 'q'");
+            message = Console.ReadLine();
+            if (message == "q") { break; }
+            bool isBigger = isLengthBiggerThanOrEqualSix(message);
+            if (isBigger)
+            {
+                int count = 0;
+                count += haveLetter(message) ? 1 : 0;
+                count += haveNumber(message) ? 1 : 0;
+                count += haveAlphanumeric(message) ? 1 : 0;
+                // 1 tip 2 tip 3 tip sayımını count üzerinden gerçekleştirdim
+                switch (count)
+                {
+                    case 0: undefinedPassword(); break;
+                    case 1: weakPassword(); break;
+                    case 2: middlePassword(); break;
+                    case 3: strongPassword(); break;
+                    default: undefinedPassword(); break;
+                }
+            }
+            else { shortPassword(); }
+        }
+    }
+
+    private static bool haveAlphanumeric(string message)
+    {
+        foreach (char letter in message) { if (char.IsPunctuation(letter)) { return true; } } return false;
+    }
+
+    private static bool haveNumber(string message)
+    {
+        foreach (char letter in message) { if (char.IsNumber(letter)) { return true; } } return false;
+    }
+
+    private static bool haveLetter(string message)
+    {
+        foreach (char letter in message) { if (char.IsLetter(letter)) { return true; } } return false;
+    }
+
+    private static bool isLengthBiggerThanOrEqualSix(string message)
+    {
+        return (message != null && message.Length >= 6);
+    }
+
+    private static void strongPassword()
+    {
+        Console.WriteLine("Strong");
+    }
+    private static void middlePassword()
+    {
+        Console.WriteLine("Middle");
+    }
+    private static void weakPassword()
+    {
+        Console.WriteLine("Weak");
+    }
+    private static void shortPassword()
+    {
+        Console.WriteLine("Password must be bigger than 6");
+    }
+
+    private static void undefinedPassword()
+    {
+        Console.WriteLine("Password must be bigger than 6");
+    }
+}
