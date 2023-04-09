556e5c8c9ed350e82ae61ace556ed6626a53f2c4
ahmet taha mücasiroğlu <48521773+tahamucasiroglu@users.noreply.github.com>
2023-04-09 16:04:43 +0300

    Add files via upload

---
+++ Events/CustomEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class KeyCreatedEventArgs : EventArgs
+    {
+        public string KeyName { get; set; }
+    }
+    public class KeyService
+    {
+        public event EventHandler<KeyCreatedEventArgs> KeyBuild;
+        public void CreateKey(string key)
+        {
+            if (KeyBuild != null && key.Length == 1)
+            {
+                KeyCreatedEventArgs arg = new KeyCreatedEventArgs
+                {
+                    KeyName = key,
+                };
+                KeyBuild(this, arg);
+            }
+        }
+        
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,31 @@
+﻿using Events;
+
+internal class Program
+{
+    static public void KeyServiceFunc(object? sender, KeyCreatedEventArgs e)
+    {
+        switch(e.KeyName)
+        {
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "0":
+                Console.WriteLine("sayı olmaz"); break;
+        }
+        Console.WriteLine($"{e.KeyName} basıldı.");
+
+    }
+
+    private static void Main(string[] args)
+    {
+        KeyService keyService = new KeyService();
+        keyService.KeyBuild += KeyServiceFunc;
+        keyService.CreateKey(Console.ReadLine());
+    }
+}
